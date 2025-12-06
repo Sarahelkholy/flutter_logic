e224f7731283675f80274e3b348feedfc7f709d3
sarah elkholy <saraelkholyarmy03@gmail.com>
2025-12-06 21:46:35 +0200

    - created image selection - added laptops assets

--- lib/screens/image_selection.dart
+++ lib/screens/image_selection.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
 
-class ImageSelection extends StatelessWidget {
+class ImageSelection extends StatefulWidget {
   const ImageSelection({super.key});
 
   @override
+  State<ImageSelection> createState() => _ImageSelectionState();
+}
+
+class _ImageSelectionState extends State<ImageSelection> {
+  List<String> images = [
+    'assets/laptops/labtop1.png',
+    'assets/laptops/labtop3.png',
+    'assets/laptops/labtop2.png',
+    'assets/laptops/labtop4.png',
+  ];
+  int selectedIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.red);
+    return Scaffold(
+      backgroundColor: Color(0xfffdee00),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            SizedBox(height: 80),
+
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.asset(images[selectedIndex], fit: BoxFit.cover),
+            ),
+            SizedBox(height: 20),
+
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(images.length, (index) {
+                  return Container(
+                    height: 100,
+                    width: 100,
+                    margin: EdgeInsets.symmetric(horizontal: 3),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+
+                      borderRadius: BorderRadius.circular(4),
+                      border: Border.all(
+                        color: selectedIndex == index
+                            ? Colors.blue
+                            : Colors.white,
+                      ),
+                    ),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      child: Image.asset(images[index]),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
