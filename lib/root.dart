f5328597e63b374c480a929525b1930187ee1101
sarah elkholy <saraelkholyarmy03@gmail.com>
2025-12-06 20:10:46 +0200

    - added animals assets - created single selection

---
+++ lib/root.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_logic/screens/multi_image_selection.dart';
+import 'package:flutter_logic/screens/single_selection.dart';
+import 'package:flutter_logic/screens/toggle_selection.dart';
+
+class Root extends StatefulWidget {
+  const Root({super.key});
+
+  @override
+  State<Root> createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  final PageController controller = PageController();
+  List<Widget> pages = [
+    SingleSelection(),
+    MultiImageSelection(),
+    ToggleSelection(),
+  ];
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: controller,
+        onPageChanged: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        children: pages,
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 20, vertical: 60),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  controller.jumpToPage(selectedIndex - 1);
+                });
+              },
+              child: Container(
+                height: 40,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Icon(Icons.arrow_back, color: Colors.black),
+              ),
+            ),
+            SizedBox(width: 20),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  controller.jumpToPage(selectedIndex + 1);
+                });
+              },
+              child: Container(
+                height: 40,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(color: Colors.black),
+                child: Row(
+                  children: [
+                    Text(
+                      'Next Page',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Icon(Icons.arrow_forward, color: Colors.white),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/multi_image_selection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MultiImageSelection extends StatelessWidget {
+  const MultiImageSelection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.red);
+  }
+}
